e19351a2f88f809dedf07a262137cb999b52f583
afthompson <andrewft@udel.edu>
2019-03-03 19:29:11 -0500

    added the Lab02 file

---
+++ Lab02/src/Animal.java
@@ -0,0 +1,18 @@
+//Andrew Thompson
+//CISC275081
+//Due 3/3/2019
+
+public class Animal {
+	int numLegs;
+	String name;
+	Animal(String name, int numLegs){
+		this.name = name;
+		this.numLegs = numLegs;
+	}
+	public int getLegs() {
+		return numLegs;
+	}
+	public String toString() {
+		return name+": "+numLegs+" legs";
+	}
+}
---
+++ Lab02/src/Dog.java
@@ -0,0 +1,10 @@
+//Andrew Thompson
+//CISC275081
+//Due 3/3/2019
+
+public class Dog extends Animal{
+	Dog(String name, int numLegs){
+		super(name,numLegs);
+	}
+
+}
